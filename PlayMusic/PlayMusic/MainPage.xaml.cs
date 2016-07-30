7d0173034562b0a982b9e1fc44a746cd74b4c2ff
Yavuz Gedik <yavuzgedik@outlook.com>
2016-07-30 21:00:39 +0300

    Updated Project

--- PlayMusic/PlayMusic/MainPage.xaml.cs
+++ PlayMusic/PlayMusic/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Playback;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,60 @@ namespace PlayMusic
         public MainPage()
         {
             this.InitializeComponent();
+            volSlider.Value = 10;
+        }
+
+        async void PlayMusic()
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                Uri pathUri = new Uri("ms-appx:///Assets/Music/tailtoddle_lo.mp3");
+
+                song.Source = pathUri;
+                song.Play();
+            });
+        }
+
+        async void PauseMusic()
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                song.Pause();
+            });
+        }
+
+        async void StopMusic()
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                song.Stop();
+            });
+        }
+        async void volChanged(double vol)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                song.Volume = vol;
+            });
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayMusic();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            PauseMusic();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopMusic();
+        }
+        private void volSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            volChanged(volSlider.Value/100);
         }
     }
 }
