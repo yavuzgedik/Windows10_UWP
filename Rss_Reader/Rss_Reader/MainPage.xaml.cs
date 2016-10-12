5406998cecd63e122ce231c2b831425acb932ebb
Yavuz Gedik <yavuzgedik@outlook.com>
2016-10-12 03:08:41 +0300

    basşc rss reader ok

--- Rss_Reader/Rss_Reader/MainPage.xaml.cs
+++ Rss_Reader/Rss_Reader/MainPage.xaml.cs
@@ -1,30 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.Web.Syndication;
 
 namespace Rss_Reader
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        // Sample Address: http://www.izsu.gov.tr/Pages/rss.aspx?rssId=3
+        private async void OkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri(UrlBox.Text);
+
+            SyndicationClient client = new SyndicationClient();
+            SyndicationFeed feed = await client.RetrieveFeedAsync(uri);
+
+            if (feed != null)
+            {
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    listBox.Items.Add(item.Title.Text  + " - " + item.Summary.Text);
+                }
+            }
         }
     }
 }
